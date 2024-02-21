0022df841335659e04842a9a640ec0ef76e0039d
ritatanght <doggymint@gmail.com>
2024-02-21 17:54:14 -0500

    Add query for search and execute on button click

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,16 +2,44 @@ import { useState } from "react";
 import { useLazyQuery, gql } from "@apollo/client";
 import Link from "./Link";
 
-const data = null;
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      id
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Search = () => {
   const [searchFilter, setSearchFilter] = useState("");
+  // useLazyQuery performs query like useQuery but it must be execute manually
+  const [executeSearch, { data }] = useLazyQuery(FEED_SEARCH_QUERY);
   return (
     <>
       <div>
         Search
         <input type="text" onChange={(e) => setSearchFilter(e.target.value)} />
-        <button>OK</button>
+        <button
+          onClick={() => executeSearch({ variables: { filter: searchFilter } })}
+        >
+          OK
+        </button>
       </div>
       {data &&
         data.feed.links.map((link, index) => (
