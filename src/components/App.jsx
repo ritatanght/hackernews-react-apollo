2686af4f05a8ad65859b98036c21f11740cf1d73
ritatanght <doggymint@gmail.com>
2024-02-20 22:08:28 -0500

    Add header for navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
 import "../styles/App.css";
 import CreateLink from "./CreateLink";
 import LinkList from "./LinkList";
+import Header from "./Header";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <CreateLink />;
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList />} />
+          <Route path="/create" element={<CreateLink />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
