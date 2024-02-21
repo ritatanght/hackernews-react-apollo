243d6b1d1074d572c85fb314f46c1bda11fb87a6
ritatanght <doggymint@gmail.com>
2024-02-21 13:17:04 -0500

    Add token to header for request sent to server

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
-import "./styles/index.css";
 import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./components/App.jsx";
+import "./styles/index.css";
+import { AUTH_TOKEN } from "./constants";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
+// This middleware will be invoked every time ApolloClient sends a request to the server
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
