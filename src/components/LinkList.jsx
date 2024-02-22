00f88e5169a761904a019743e9aae1863c938987
ritatanght <doggymint@gmail.com>
2024-02-21 23:11:01 -0500

    Subscribe to New links

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -25,8 +25,51 @@ export const FEED_QUERY = gql`
   }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
 const LinkList = () => {
-  const { data } = useQuery(FEED_QUERY);
+  const { data, loading, error, subscribeToMore } = useQuery(FEED_QUERY);
+
+  subscribeToMore({
+    // subscription document is where we define our subscription
+    document: NEW_LINKS_SUBSCRIPTION,
+    // updateQuery filed is used to update the cache
+    updateQuery: (prev, { subscriptionData }) => {
+      console.log("prev", prev);
+      if (!subscriptionData.data) return prev;
+      const newLink = subscriptionData.data.newLink;
+      const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+      if (exists) return prev;
+
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename,
+        },
+      });
+    },
+  });
+
   return (
     <div>
       {data && (
