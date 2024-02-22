5550f35c75f26b832b020894b05513c366e60f63
ritatanght <doggymint@gmail.com>
2024-02-21 22:56:31 -0500

    Configure subscriptions endpoint

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter } from "react-router-dom";
+import { split } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 import App from "./components/App.jsx";
 import "./styles/index.css";
@@ -28,8 +31,33 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+});
+
+/* split is used to “route” a request to a specific middleware link
+ It takes three arguments: 
+ 1) test function that returns a boolean
+ 2) If function returns TRUE, request will be forwarded to this link
+ 3) If function returns FALSE, request will be forwarded to this link
+*/
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
 
