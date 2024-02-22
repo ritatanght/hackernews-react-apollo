0ef5d200f5486dadafe12b57d7d48f504a455044
ritatanght <doggymint@gmail.com>
2024-02-22 12:47:01 -0500

    Render conditionally on values

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,5 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { LINKS_PER_PAGE } from "../constants";
 import Link from "./Link";
@@ -84,8 +84,19 @@ const getQueryVariables = (isNewPage, page) => {
   return { take, skip, orderBy };
 };
 
+const getLinksToRender = (isNewPage, data) => {
+  if (isNewPage) {
+    return data.feed.links;
+  }
+  const rankedLinks = data.feed.links.slice();
+  rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length);
+  return rankedLinks;
+};
+
 const LinkList = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+  
   const isNewPage = location.pathname.includes("new");
   const pageIndexParams = location.pathname.split("/");
   const page = parseInt(pageIndexParams[pageIndexParams.length - 1]);
@@ -119,15 +130,42 @@ const LinkList = () => {
   });
 
   return (
-    <div>
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
       {data && (
         <>
-          {data.feed.links.map((link, index) => (
-            <Link key={link.id} link={link} index={index} />
+          {getLinksToRender(isNewPage, data).map((link, index) => (
+            <Link key={link.id} link={link} index={index + pageIndex} />
           ))}
+          {isNewPage && (
+            <div className="flex ml4 mv3 gray">
+              <div
+                className="pointer mr2"
+                onClick={() => {
+                  if (page > 1) {
+                    navigate(`/new/${page - 1}`);
+                  }
+                }}
+              >
+                Previous
+              </div>
+              <div
+                className="pointer"
+                onClick={() => {
+                  if (page <= data.feed.count / LINKS_PER_PAGE) {
+                    const nextPage = page + 1;
+                    navigate(`/new/${nextPage}`);
+                  }
+                }}
+              >
+                Next
+              </div>
+            </div>
+          )}
         </>
       )}
-    </div>
+    </>
   );
 };
 
